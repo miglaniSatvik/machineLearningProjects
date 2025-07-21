bde630b0207d21b8ad5b409ccdd29dfccb3c29a3
void-desperado <satvikmiglani07@gmail.com>
2025-07-21 09:07:45 +0000

    Updated irisClassifier.py

--- classifyingIrisSpecies/webapp.py
+++ classifyingIrisSpecies/webapp.py
@@ -1,8 +1,12 @@
 import streamlit as st
 import pickle
 import numpy as np
+import os
 
-with open("classifier.pkl", "rb") as model_file:
+HERE = os.path.dirname(__file__)  
+MODEL_FP = os.path.join(HERE, "classifier.pkl")
+
+with open(MODEL_FP, "rb") as model_file:
     model = pickle.load(model_file)
 
 st.title("Iris Species Classifier")
@@ -16,4 +20,9 @@ petal_width = st.slider("Petal Width (cm)", min_value=0.1, max_value=2.5, step=0
 if st.button("Predict"):
     features = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
     prediction = model.predict(features)
-    st.write(f"Predicted Iris Species: {prediction[0]}")
+    if (prediction[0]==0):
+        st.write(f"Predicted Iris Species: {"Iris-setosa"}")
+    elif(prediction[0]==1):
+        st.write(f"Predicted Iris Species: {"Iris-versicolor"}")
+    elif(prediction[0]==2):
+        st.write(f"Predicted Iris Species: {"Iris-verginica"}")
